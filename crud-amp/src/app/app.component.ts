28bf5666b1ad990f5c163dc0fa129c64e930ad6e
AbisurDiazR <abisurdiazramirez@gmail.com>
2020-05-30 02:18:52 -0500

    Creacion del login

--- crud-amp/src/app/app.component.ts
+++ crud-amp/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var jQuery: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'crud-amp';
+export class AppComponent implements OnInit{
+  title = 'Leomary';
+
+  ngOnInit(): void{
+    (function ($) {
+      $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: (target.offset().top - 71)
+            }, 1000, "easeInOutExpo");
+            return false;
+          }
+        }
+      });
+    })(jQuery);
+  }
 }
