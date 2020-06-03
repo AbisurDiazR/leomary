1c0ba6116e66a77b15b778d6e0928aeb5041036c
AbisurDiazR <abisurdiazramirez@gmail.com>
2020-06-03 02:46:49 -0500

    Configuramos los servicios de departamentos y creamos el servicio categorias

--- crud-amp/src/app/services/departamentos.service.ts
+++ crud-amp/src/app/services/departamentos.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartamentosService {
 
-  constructor() { }
+  URL = 'http://localhost/backend/';
+
+  constructor(private http: HttpClient) { }
+
+  //Agregamos un nuevo departamento a la base de datos
+  agregarDepartamento(departamento){
+    return this.http.post(`${this.URL}agregarDepartamento.php`,JSON.stringify(departamento));
+  }
+
+  //obtenemos los departamenos desde la base de datos
+  obtenerDepartamentos(){
+    return this.http.get(`${this.URL}obtenerDepartamentos.php`);
+  }
 }
