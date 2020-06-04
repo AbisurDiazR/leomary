fe7253fdd1d1cd1e71351807b182417b836cfb6e
AbisurDiazR <abisurdiazramirez@gmail.com>
2020-06-04 00:13:45 -0500

    Metodo para subir imagenes

--- crud-amp/src/app/components/adminproducts/adminproducts.component.ts
+++ crud-amp/src/app/components/adminproducts/adminproducts.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DepartamentosService } from 'src/app/services/departamentos.service';
+import { CategoriasService } from 'src/app/services/categorias.service';
+import { UploadimageService } from 'src/app/services/uploadimage.service';
 
 @Component({
   selector: 'app-adminproducts',
@@ -7,7 +9,7 @@ import { DepartamentosService } from 'src/app/services/departamentos.service';
   styleUrls: ['./adminproducts.component.scss']
 })
 export class AdminproductsComponent implements OnInit {
-
+  //Variables para los departamentos
   departamentos = null;
 
   departamento = {
@@ -15,12 +17,30 @@ export class AdminproductsComponent implements OnInit {
     nombre_departamento: null
   };
 
-  constructor(private departamentosService: DepartamentosService) { }
+  //Variables para las categorias
+  categorias = null;
+
+  categoria = {
+    id_categoria: null,
+    nombre_categoria: null
+  }
+
+  //variable imagen producto
+  archivoImagen = {
+    nombre: null,
+    nombreArchivo: null,
+    base64textString: null
+  }
+
+  constructor(private departamentosService: DepartamentosService, private categoriasService: CategoriasService
+    , private uploadService: UploadimageService) { }
 
   ngOnInit(): void {
     this.obtenerDepartamentos();
+    this.obtenerCategorias();
   }
 
+  //metodos para los departamentos
   obtenerDepartamentos(){
     this.departamentosService.obtenerDepartamentos().subscribe(
       result => this.departamentos = result
@@ -33,6 +53,54 @@ export class AdminproductsComponent implements OnInit {
         if (datos['resultado']=='OK') {
           alert(datos['mensaje']);
           this.obtenerDepartamentos();
+        }
+      }
+    );
+  }
+
+  //metodos para las categorias
+  obtenerCategorias(){
+    this.categoriasService.obtenerCategorias().subscribe(
+      result => this.categorias = result
+    );
+  }
+
+  agregarCategoria(){
+    this.categoriasService.agregarCategoria(this.categoria).subscribe(
+      datos => {
+        if (datos['mensaje']=='OK') {
+          alert(datos['mensaje']);
+          this.obtenerCategorias();
+        }
+      }
+    );
+  }
+
+  //metodos para la seleccion del archivo de imagen
+  seleccionarArchivo(event){
+    var files = event.target.files;
+    var file = files[0];
+    this.archivoImagen.nombreArchivo = file.name;
+
+    if (files && file) {
+      var reader = new FileReader();
+      reader.onload = this._handleReaderLoaded.bind(this);
+      reader.readAsBinaryString(file);
+    }
+  }
+
+  _handleReaderLoaded(readerEvent){
+    var binaryString = readerEvent.target.result;
+    this.archivoImagen.base64textString = btoa(binaryString);
+  }
+
+  //metodo para subir imagen
+  upload(){
+    console.log(this.archivoImagen);
+    this.uploadService.uploadImagen(this.archivoImagen).subscribe(
+      datos => {
+        if (datos['resultado']=='OK') {
+          alert(datos['mensaje']);
         }
       }
     );
--- crud-amp/src/app/services/categorias.service.ts
+++ crud-amp/src/app/services/categorias.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriasService {
 
-  constructor() { }
+  URL = 'http://localhost/backend/';
+
+  constructor(private hhtp: HttpClient) { }
+
+  //agregamos la categoria a la base de datos
+  agregarCategoria(categoria){
+    return this.hhtp.post(`${this.URL}agregarCategoria.php`,JSON.stringify(categoria));
+  }
+
+  //obtenemos las categorias de la base de datos
+  obtenerCategorias(){
+    return this.hhtp.get(`${this.URL}obtenerCategorias.php`);
+  }
 }
